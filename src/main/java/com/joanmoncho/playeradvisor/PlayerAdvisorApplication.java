901e447c40f427e5dadddc2b12f6102c92348bc0
Joan Moncho <joanmonvi@gmail.com>
2022-11-24 15:34:25 +0100

    commit springcore

--- src/main/java/com/joanmoncho/playeradvisor/PlayerAdvisorApplication.java
+++ src/main/java/com/joanmoncho/playeradvisor/PlayerAdvisorApplication.java
@@ -8,7 +8,7 @@ public class PlayerAdvisorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlayerAdvisorApplication.class, args);
-        System.out.println();
+        System.out.println("hola");
 
 
     }
