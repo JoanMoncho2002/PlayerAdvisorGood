472efd4590f4a7a9157eea57b0a2382892565e23
Joan Moncho <joanmonvi@gmail.com>
2022-11-24 13:49:08 +0100

    segundo commit

--- src/main/java/com/joanmoncho/playeradvisor/PlayerAdvisorApplication.java
+++ src/main/java/com/joanmoncho/playeradvisor/PlayerAdvisorApplication.java
@@ -8,6 +8,8 @@ public class PlayerAdvisorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlayerAdvisorApplication.class, args);
+
+        String ejemplo;
     }
 
 }
